74705d9fc1c45a9e2cf47394a568965e8dd9ec60
BilalRehman08 <bilalrehman080808@gmail.com>
2022-04-20 13:40:55 +0500

    adding constant values

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,2 +1,15 @@
 /// Constants contains the keys used for streaming events and bytes
-class Constants {}
+class Constants {
+  /// Event for when network state changes to Wifi
+  static const wifi = 0xFF;
+
+  /// Event for when network state changes to cellular/mobile data
+  static const cellular = 0xEE;
+
+  /// Event for when network is disconnected
+  static const disconnected = 0xDD;
+
+  /// Event for when network state is a state you do not
+  /// support (e.g VPN or Ethernet on Android)
+  static const unknown = 0xCC;
+}
