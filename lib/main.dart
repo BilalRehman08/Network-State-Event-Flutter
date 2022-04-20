5cbf98f9c453715a3590cdc4ae2195ab6f7690d8
BilalRehman08 <bilalrehman080808@gmail.com>
2022-04-20 13:29:30 +0500

    using network stream widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'network_stream_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(title: 'Events Channel Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          const NetworkStreamWidget(),
+          Expanded(child: Container()),
+        ],
+      ),
+    );
   }
 }
