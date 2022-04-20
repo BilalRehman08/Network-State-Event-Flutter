2da169c12dd954756f2e91f14811c58cd306cdb5
BilalRehman08 <bilalrehman080808@gmail.com>
2022-04-20 13:47:01 +0500

    mapping connections to color object

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../constants/constants.dart';
+
 /// Connection is an enum of supported network states
 enum Connection {
   /// When connection state is [Constants.wifi]
@@ -13,28 +17,48 @@ enum Connection {
   unknown
 }
 
+/// converts the network events to the appropriate values of
+/// the [Connection] enum
+Connection intToConnection(int connectionInt) {
+  var connection = Connection.unknown;
+  switch (connectionInt) {
+    case Constants.wifi:
+      connection = Connection.wifi;
+      break;
+    case Constants.cellular:
+      connection = Connection.cellular;
+      break;
+    case Constants.disconnected:
+      connection = Connection.disconnected;
+      break;
+    case Constants.unknown:
+      connection = Connection.unknown;
+      break;
+  }
+  return connection;
+}
 
-/// converts the network events to the appropriate Color
-// Color getConnectionColor(Connection connection) {
-//   var color = Colors.red[900];
-//   switch (connection) {
-//     case Connection.wifi:
-//       color = Colors.green[800];
-//       break;
-//     case Connection.cellular:
-//       color = Colors.blue[900];
-//       break;
-//     case Connection.disconnected:
-//       color = Colors.red[900];
-//       break;
-//     case Connection.unknown:
-//       color = Colors.red[900];
-//       break;
-//   }
-//   return color!;
-// }
+//  converts the network events to the appropriate Color
+Color getConnectionColor(Connection connection) {
+  var color = Colors.red[900];
+  switch (connection) {
+    case Connection.wifi:
+      color = Colors.green[800];
+      break;
+    case Connection.cellular:
+      color = Colors.blue[900];
+      break;
+    case Connection.disconnected:
+      color = Colors.red[900];
+      break;
+    case Connection.unknown:
+      color = Colors.red[900];
+      break;
+  }
+  return color!;
+}
 
-/// converts the network events to the appropriate user-readable strings
+// / converts the network events to the appropriate user-readable strings
 // String getConnectionMessage(Connection connection) {
 //   var msg = 'Unknown connection';
 //   switch (connection) {
