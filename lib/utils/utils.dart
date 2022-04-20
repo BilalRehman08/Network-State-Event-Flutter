e56a925f40b5e801309e5ec254c1a79989281139
BilalRehman08 <bilalrehman080808@gmail.com>
2022-04-20 13:42:47 +0500

    enum Connection

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,19 @@
+/// Connection is an enum of supported network states
+enum Connection {
+  /// When connection state is [Constants.wifi]
+  wifi,
+
+  /// When connection state is [Constants.cellular]
+  cellular,
+
+  /// When connection state is [Constants.disconnected]
+  disconnected,
+
+  /// When connection state is [Constants.unknown]
+  unknown
+}
+
+
 /// converts the network events to the appropriate Color
 // Color getConnectionColor(Connection connection) {
 //   var color = Colors.red[900];
