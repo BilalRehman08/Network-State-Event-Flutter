7aa13c8964fa6f56d0df1c6e6dcb4c0520123bac
BilalRehman08 <bilalrehman080808@gmail.com>
2022-04-20 13:41:33 +0500

    utils

---
+++ lib/utils/utils.dart
@@ -0,0 +1,39 @@
+/// converts the network events to the appropriate Color
+// Color getConnectionColor(Connection connection) {
+//   var color = Colors.red[900];
+//   switch (connection) {
+//     case Connection.wifi:
+//       color = Colors.green[800];
+//       break;
+//     case Connection.cellular:
+//       color = Colors.blue[900];
+//       break;
+//     case Connection.disconnected:
+//       color = Colors.red[900];
+//       break;
+//     case Connection.unknown:
+//       color = Colors.red[900];
+//       break;
+//   }
+//   return color!;
+// }
+
+/// converts the network events to the appropriate user-readable strings
+// String getConnectionMessage(Connection connection) {
+//   var msg = 'Unknown connection';
+//   switch (connection) {
+//     case Connection.wifi:
+//       msg = 'Connected to Wifi';
+//       break;
+//     case Connection.cellular:
+//       msg = 'Connected to mobile data';
+//       break;
+//     case Connection.disconnected:
+//       msg = 'Offline';
+//       break;
+//     case Connection.unknown:
+//       msg = 'Unknown connection';
+//       break;
+//   }
+//   return msg;
+// }
