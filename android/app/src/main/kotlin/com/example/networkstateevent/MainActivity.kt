ae7dadd6c212a1dccc91f96a1d5ea1d8cd44eed0
BilalRehman08 <bilalrehman080808@gmail.com>
2022-04-20 14:09:47 +0500

    creates event channel in main activity kotlin

--- android/app/src/main/kotlin/com/example/networkstateevent/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/networkstateevent/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.networkstateevent
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
 
 class MainActivity: FlutterActivity() {
+    private val networkEventChannel = "platform_channel_events/connectivity"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, networkEventChannel)
+            .setStreamHandler(NetworkStreamHandler(this))
+
+    }
 }
